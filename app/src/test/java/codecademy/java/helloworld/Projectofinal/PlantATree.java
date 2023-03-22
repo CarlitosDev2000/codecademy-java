c4f2acd3fb9c661b227e6298f5b79ed599f4e7a5
Carlos Cordova <carlos.cordova.perez.2000@gmail.com>
2023-03-21 21:33:44 -0500

    hello world: project 1

---
+++ app/src/test/java/codecademy/java/helloworld/Projectofinal/PlantATree.java
@@ -0,0 +1,26 @@
+package codecademy.java.helloworld.Projectofinal;
+
+public class PlantATree {
+    public static void main(String[]args){
+        //We’re going to introduce ourselves and print a tree to the screen.
+
+        System.out.println("Hey there, I’m Carlos and I’m learning to code in Java!");
+
+        System.out.println("I’m going to plant a tree today");
+
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+
+        System.out.println(" ");
+
+        System.out.println("* ** *");
+        System.out.println("******");
+        System.out.println(" **** ");
+        System.out.println("  ** ");
+        System.out.println("  ** ");
+    }
+}
+
