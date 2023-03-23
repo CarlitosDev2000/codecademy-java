923698f39431f2d077ef2004de9861e35e308d6f
Carlos Cordova <carlos.cordova.perez.2000@gmail.com>
2023-03-23 00:11:46 -0500

    Variables:

--- app/src/test/java/codecademy/java/variables/exercise3/ExerciseThreeTest.java
+++ app/src/test/java/codecademy/java/variables/exercise3/ExerciseThreeTest.java
@@ -1,4 +1,8 @@
 package codecademy.java.variables.exercise3;
 
 public class ExerciseThreeTest {
+    public static void main(String[] args) {
+        char expectedGrade ='A';
+        System.out.println(expectedGrade);
+    }
 }
---
+++ app/src/test/java/codecademy/java/variables/exercise5/ExerciseFiveTest.java
@@ -0,0 +1,9 @@
+package codecademy.java.variables.exercise5;
+
+public class ExerciseFiveTest {
+    public static void main(String[] args) {
+        boolean intsCanHoldDecimals = false;
+        System.out.println(intsCanHoldDecimals);
+    }
+    //recontra simple el boolean
+}
