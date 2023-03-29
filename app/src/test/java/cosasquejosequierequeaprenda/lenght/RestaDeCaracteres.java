f47fa353518eaad404117c62b5cdefe0c91d23ef
Carlos Cordova <carlos.cordova.perez.2000@gmail.com>
2023-03-28 20:54:38 -0500

    resta de caracteres

--- app/src/test/java/cosasquejosequierequeaprenda/lenght/RestaDeCaracteres.java
+++ app/src/test/java/cosasquejosequierequeaprenda/lenght/RestaDeCaracteres.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class RestaDeCaracteres {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int intentos = 2;
+
         System.out.println("ingrese el primer nombre: ");
         String palabraUno = sc.nextLine();
 
@@ -24,9 +24,9 @@ public class RestaDeCaracteres {
         if (resta == 0){
             System.out.println("la palabra " + palabraUno + " tiene la misma cantidad de caracteres que la palabra " + palabraDos);
         } else if (resta > 0) {
-            System.out.println("la resta de los dos caracteres es igual a " + resta);
+            System.out.println("la palabra " + palabraUno + "tiene " + resta + " mas caracteres que la palabra " + palabraDos);
         } else if (resta < 0) {
-            System.out.println("la resta de los dos caracteres es igual a " + Math.abs(resta));
+            System.out.println("la palabra " + palabraDos + " tiene " + Math.abs(resta) + " mas caracteres que la palabra " + palabraUno);
         }
 
 
